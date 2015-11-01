886db391f0f6c72cc3adf48407aad34ce16df2dc
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-11-01 06:57:46 +0000

    Create main

--- RUBY/appliances/main.rb
+++ RUBY/appliances/main.rb
@@ -1,4 +1,16 @@
 require './lib/microwave'
+require './lib/dishwasher'
 
 mic_1 = Microwave.new(door: :closed, power: :on, timer: :on)
-mic_1.start
+mic_2 = Microwave.new(door: :open, power: :on, timer: :off)
+
+mic_1.start # Starting...
+mic_2.start # Could not...
+
+p "========================="
+
+dishwasher_1 = Dishwasher.new(door: :closed, power: :on)
+dishwasher_2 = Dishwasher.new(door: :open, power: :on)
+
+dishwasher_1.start # Starting...
+dishwasher_2.start # Could not...
