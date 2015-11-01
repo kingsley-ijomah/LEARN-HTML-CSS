3a7f8e3f2b465a4709ccc54f8f9cbeb293e5907d
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-11-01 06:57:37 +0000

    Add microwave

--- RUBY/appliances/lib/microwave.rb
+++ RUBY/appliances/lib/microwave.rb
@@ -1,46 +1,24 @@
-class Microwave
-	def initialize(door:, power:, timer:)
-		@door = door
-		@power = power
+require './lib/appliances'
+
+class Microwave < Appliances
+	def initialize door:, power:, timer:
+		super door: door, power: power
 		@timer = timer
-		@started = false
 	end
 
 	def start
-		if can_start?
-			p "Starting..."
-			@started = true
+		if  startable?
+			p "Microwave Starting..."
 		else
-			p "Could not start"
+			p "Microwave Could not start"
 		end
 	end
 
-	def can_start?
-		closed? && power_on?  && timer_on?
-	end
-
-	def started?
-		@started == true
-	end
-
-	def closed?
-		@door == :closed
-	end
-
-	def power_on?
-		@power == :on
+	def startable?
+		closed? && power_on? && timer_on?
 	end
 
 	def timer_on?
 		@timer == :on
-	end
-
-	def stop
-		if started?
-			p "Stopping..."
-			@started = false
-		else
-			p "Already off..."
-		end
 	end
 end
