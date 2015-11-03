136e27ac293dc94e8cac97be8bb2d6501794393e
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-11-03 14:04:06 +0000

    Adding main file

--- RUBY/appliances/main.rb
+++ RUBY/appliances/main.rb
@@ -1,19 +1,16 @@
 require './lib/microwave'
+require './lib/dishwasher'
 
-data1 = {door: :closed, timer: :on, power: :on}
-data2 = {door: :open, timer: :on, power: :on}
-data3 = {door: :closed, timer: :off, power: :on}
-data4 = {door: :closed, timer: :on, power: :on}
-data5 = {door: :closed, timer: :on, power: :off}
-
-instance1 = Microwave.create(data1)
-instance2 = Microwave.create(data2)
-instance3 = Microwave.create(data3)
-instance4 = Microwave.create(data4)
-instance5 = Microwave.create(data5)
+instance1 = Microwave.create(door: :closed, timer: :on, power: :on)
+instance2 = Microwave.create(door: :open, timer: :on, power: :on)
 
 p instance1.start
 p instance2.start
-p instance3.start
-p instance4.start
-p instance5.start
+
+p "================="
+
+instance1 = Dishwasher.create(door: :closed, power: :on)
+instance2 = Dishwasher.create(door: :open, power: :on)
+
+p instance1.start
+p instance2.start
